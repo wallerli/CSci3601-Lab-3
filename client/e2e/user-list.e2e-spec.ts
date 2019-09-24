8ce22e3a96d045d5653642b924e1972aa6a6ef5e
K.K. Lamberty <lamberty@morris.umn.edu>
2019-09-24 14:56:25 -0500

    Initialize improve-test-coverage

---
+++ client/e2e/user-list.e2e-spec.ts
@@ -0,0 +1,53 @@
+import {UserPage} from './user-list.po';
+import {browser, protractor} from 'protractor';
+
+let origFn = browser.driver.controlFlow().execute;
+
+//https://hassantariqblog.wordpress.com/2015/11/09/reduce-speed-of-angular-e2e-protractor-tests/
+browser.driver.controlFlow().execute = function () {
+    let args = arguments;
+
+    // queue 100ms wait between test
+    //This delay is only put here so that you can watch the browser do its' thing.
+    //If you're tired of it taking long you can remove this call
+    origFn.call(browser.driver.controlFlow(), function () {
+        return protractor.promise.delayed(100);
+    });
+
+    return origFn.apply(browser.driver.controlFlow(), args);
+};
+
+describe('angular-spark-lab', () => {
+    let page: UserPage;
+
+    beforeEach(() => {
+        page = new UserPage();
+    });
+
+    it('should get and highlight User Name attribute ', () => {
+        page.navigateTo();
+        expect(page.getUserTitle()).toEqual('Users');
+    });
+
+    it('should type something in filter name box and check that it returned correct element', () => {
+        page.navigateTo();
+        page.typeAName("t");
+        expect(page.getUniqueUser("kittypage@surelogic.com")).toEqual("Kitty Page");
+        page.backspace();
+        page.typeAName("lynn")
+        expect(page.getUniqueUser("lynnferguson@niquent.com")).toEqual("Lynn Ferguson");
+    });
+
+    it('should click on the age 27 times and return 3 elements', () => {
+        page.navigateTo();
+        page.getUserByAge();
+        for (let i = 0; i < 27; i++) {
+            page.selectUpKey();
+        }
+
+        expect(page.getUniqueUser("stokesclayton@momentia.com")).toEqual("Stokes Clayton");
+
+        expect(page.getUniqueUser("merrillparker@escenta.com")).toEqual("Merrill Parker");
+
+    });
+});
---
+++ client/src/app/users/user-list.service.ts
@@ -0,0 +1,23 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+import {Observable} from "rxjs";
+
+import {User} from './user';
+import {environment} from "../../environments/environment";
+
+@Injectable()
+export class UserListService {
+    readonly userUrl: string = environment.API_URL + "users";
+
+    constructor(private httpClient: HttpClient) {
+    }
+
+    getUsers(): Observable<User[]> {
+        return this.httpClient.get<User[]>(this.userUrl);
+    }
+
+    getUserById(id: string): Observable<User> {
+        return this.httpClient.get<User>(this.userUrl + "/" + id);
+    }
+}
