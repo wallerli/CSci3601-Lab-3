bdfc4a1e65a0f9eddb019b9ca456878e5dcd7ef0
K.K. Lamberty <lamberty@morris.umn.edu>
2019-09-24 14:56:27 -0500

    Initialize dependabot/gradle/com.moowork.node-1.3.1

---
+++ client/src/app/custom.module.ts
@@ -0,0 +1,71 @@
+import {NgModule,} from '@angular/core';
+import {CommonModule,} from '@angular/common';
+
+import {CovalentCommonModule, CovalentLayoutModule, CovalentStepsModule} from '@covalent/core';
+
+import {
+  MatExpansionModule,
+  MatListModule,
+  MatTooltipModule,
+  MdButtonModule,
+  MdCardModule,
+  MdIconModule,
+  MdInputModule,
+  MdMenuModule,
+  MdSidenavModule,
+  MdToolbarModule
+} from '@angular/material';
+
+import {FlexLayoutModule,} from '@angular/flex-layout';
+
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+const FLEX_LAYOUT_MODULES: any[] = [
+  FlexLayoutModule,
+];
+
+const ANGULAR_MODULES: any[] = [
+  BrowserAnimationsModule,
+  FormsModule,
+];
+
+const MATERIAL_MODULES: any[] = [
+  MatListModule,
+  MdButtonModule,
+  MdIconModule,
+  MdToolbarModule,
+  MdCardModule,
+  MdMenuModule,
+  MdSidenavModule,
+  MdInputModule,
+  MatExpansionModule,
+  MatTooltipModule
+];
+
+const COVALENT_MODULES: any[] = [
+  CovalentLayoutModule,
+  CovalentStepsModule,
+  CovalentCommonModule,
+];
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ANGULAR_MODULES,
+    MATERIAL_MODULES,
+    COVALENT_MODULES,
+    FLEX_LAYOUT_MODULES,
+  ],
+  declarations: [],
+  exports: [
+    ANGULAR_MODULES,
+    MATERIAL_MODULES,
+    COVALENT_MODULES,
+    FLEX_LAYOUT_MODULES,
+  ]
+})
+
+export class CustomModule {
+}
