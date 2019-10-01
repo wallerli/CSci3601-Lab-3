e390877893b85847e3768d6bb5be7502d3691139
Waller Li <lsw626313@gmai.com>
2019-10-01 14:37:54 -0500

    Remove unused comments #30

--- client/e2e/user-list.e2e-spec.ts
+++ client/e2e/user-list.e2e-spec.ts
@@ -3,13 +3,12 @@ import {browser, protractor} from 'protractor';
 
 let origFn = browser.driver.controlFlow().execute;
 
-//https://hassantariqblog.wordpress.com/2015/11/09/reduce-speed-of-angular-e2e-protractor-tests/
+// https://hassantariqblog.wordpress.com/2015/11/09/reduce-speed-of-angular-e2e-protractor-tests/
 browser.driver.controlFlow().execute = function () {
   let args = arguments;
 
-  // queue 100ms wait between test
-  //This delay is only put here so that you can watch the browser do its' thing.
-  //If you're tired of it taking long you can remove this call
+  // This delay is only put here so that you can watch the browser do its' thing.
+  // If you're tired of it taking long you can remove this call
   origFn.call(browser.driver.controlFlow(), function () {
     return protractor.promise.delayed(50);
   });
